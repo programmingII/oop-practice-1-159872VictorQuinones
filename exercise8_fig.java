ba99eed257548a97fd97bd8243ab0015fdb1e06f
159872VictorQuinones <46726694+159872VictorQuinones@users.noreply.github.com>
2019-02-07 13:44:33 -0700

    Add files via upload

---
+++ exercise8_fig.java
@@ -0,0 +1,14 @@
+//nombre:Victor Manuel Quiñones Pineda
+//dia:07/02/2019
+//hora: 1:34pm 
+
+public class exercise8_fig { // declaras la clase principal 
+ 
+    public static void main(String[] args) { // funcion principal de instanciacion con main 
+
+        System.out.println("   J    a   v     v  a "); 
+        System.out.println("   J   a a   v   v  a a");    //muestra un mensaje a traves del system out el cual forma unas figuras en pantallas 
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+        System.out.println(" JJ  a     a   V  a     a");
+    }
+}
