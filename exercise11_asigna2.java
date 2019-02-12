e12d3b4687b1f4edcec3c87d6515718336eb2dd2
159872VictorQuinones <46726694+159872VictorQuinones@users.noreply.github.com>
2019-02-11 21:54:57 -0700

    Add files via upload

---
+++ exercise11_asigna2.java
@@ -0,0 +1,19 @@
+//nombre:Victor Manuel Quiñones Pineda 
+//fecha: 11/02/2019
+//hora: 9:40pm
+
+
+
+public class exercise11_asigna2 { //se declara la clase principal 
+ 
+   private static final double radius = 7.5; //se declara el radio como dato privado 
+
+    public static void main(String[] args) { //se define la funcion principal con el instanciador main 
+
+        double perimeter = 2 * Math.PI * radius; //se realiza la asignacion por medio 
+        double area = Math.PI * radius * radius; //de =
+
+        System.out.println("Perimeter is = " + perimeter); // se imprime en pantalla a travez de system.out el perimeter 
+        System.out.println("Area is = " + area); // se imprime en pantalla a el area que se habia asignado anteriormente 
+    }
+}
