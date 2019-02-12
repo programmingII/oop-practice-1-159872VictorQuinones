2d1d5a7d733eab2224531891fb429fe24a5b6b04
159872VictorQuinones <46726694+159872VictorQuinones@users.noreply.github.com>
2019-02-11 21:24:30 -0700

    Add files via upload

---
+++ exercise9_jerarquia.java
@@ -0,0 +1,11 @@
+//nombre:Victor Manuel Quiñones Pineda 
+//fecha:11/02/2019
+//hora:9:14
+
+public class exercise9_jerarquia {
+ 
+    public static void main(String[] arg) { //funcion principal que contiene el main 
+
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)); //imprime en pantalla a travez del system.out
+    } // se realiza la operaciones espeificadas en los parentesis con su jerarquia de operaciones
+}
