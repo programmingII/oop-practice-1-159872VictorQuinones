a660abe63bb0b73e03df8194b508525f053a99f7
159872VictorQuinones <46726694+159872VictorQuinones@users.noreply.github.com>
2019-02-11 21:38:25 -0700

    Add files via upload

---
+++ exercise10_asigna.java
@@ -0,0 +1,12 @@
+//nombre:Victor Manuel Quiñones Pineda
+//fecha:11/02/2019
+//hora:9:26
+
+public class exercise10_asigna { //declaracion de clase publica 
+ 
+    public static void main(String[] args) { // funcion principal con instanciacion main
+
+        double pi = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)); // a double pi se le asigna el resultado de las operaciones expresadas entre parentesis   
+        System.out.println(pi); //mediante el system.out se imprime el resultado de pi 
+    }
+}
