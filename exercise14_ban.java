8c3fd559327d1fe0e48c4ac0a04139d6b0c19cbc
159872VictorQuinones <46726694+159872VictorQuinones@users.noreply.github.com>
2019-02-17 21:03:00 -0700

    Add files via upload

---
+++ exercise14_ban.java
@@ -0,0 +1,25 @@
+//nombre:Victor Manuel Quiñones Pineda 
+//fecha:17/02/2019
+//hora:8:48pm 
+
+public class exercise14_ban { //se declara la clase principal
+ 
+   public static void main(String[] args) //se declara la funcion principal con el instanciador main
+    {
+        System.out.println("* * * * * * =================================="); //por medio de system.out se imprime en pantalla lo siguiente 
+        System.out.println(" * * * * *  =================================="); //formando la siguiente figura 
+        System.out.println("* * * * * * =================================="); // parecido a la bandera de USA
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+    }
+}
