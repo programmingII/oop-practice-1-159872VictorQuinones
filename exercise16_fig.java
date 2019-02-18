b7c3fbb7c93ba263771fafad4b239fd1f55f2298
159872VictorQuinones <46726694+159872VictorQuinones@users.noreply.github.com>
2019-02-17 21:41:50 -0700

    Add files via upload

---
+++ exercise16_fig.java
@@ -0,0 +1,16 @@
+//nombre:Victor Manuel Quiñones Pineda 
+//fecha:17/02/2019
+//hora:9:33pm 
+
+
+public class exercise16_fig { //se declara la clase principal publica
+    public static void main(String[] args) //se declara la funcion principal con el instanciador main 
+    {
+        System.out.println(" +\"\"\"\"\"+ "); //se imprime en pantalla una serie de texto el cual 
+        System.out.println("[| o o |]");     //forma una figura 
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+    }
+	}
+	
