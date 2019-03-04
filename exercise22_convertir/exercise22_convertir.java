a21a63b0b00cac39e423c0a2c3ac67a1be785cd3
159872VictorQuinones <46726694+159872VictorQuinones@users.noreply.github.com>
2019-03-03 21:43:51 -0700

    Add files via upload

---
+++ exercise22_convertir/exercise22_convertir.java
@@ -0,0 +1,23 @@
+//nombre:Victor Manuel Quiñones Pineda 
+//fecha:/03/19
+//hora:
+
+import java.util.Scanner;
+public class exercise22_convertir {
+ public static void main(String[] args) 
+ {
+  Scanner sc = new Scanner(System.in);
+  long binaryNumber, decimalNumber = 0, j = 1, remainder;
+  System.out.print("Input a binary number: ");
+  binaryNumber = sc.nextLong();
+
+  while (binaryNumber != 0) 
+  {
+   remainder = binaryNumber % 10;
+   decimalNumber = decimalNumber + remainder * j;
+   j = j * 2;
+   binaryNumber = binaryNumber / 10;
+  }
+  System.out.println("Decimal Number: " + decimalNumber);
+ }
+}
