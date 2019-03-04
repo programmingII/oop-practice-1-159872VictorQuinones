b6e1231019926e6b25ec6c6c09d6d56f0cedc905
159872VictorQuinones <46726694+159872VictorQuinones@users.noreply.github.com>
2019-03-03 22:29:35 -0700

    Add files via upload

---
+++ exercise31_java/exercise31_java.java
@@ -0,0 +1,14 @@
+//nombre:Victor Manuel Quiñones Pineda 
+//fecha:/03/19
+//hora:
+
+public class exercise31_java {
+public static void main(String[] args) {
+    System.out.println("\nJava Version: "+System.getProperty("java.version"));
+    System.out.println("Java Runtime Version: "+System.getProperty("java.runtime.version"));
+    System.out.println("Java Home: "+System.getProperty("java.home"));
+    System.out.println("Java Vendor: "+System.getProperty("java.vendor"));
+    System.out.println("Java Vendor URL: "+System.getProperty("java.vendor.url"));
+    System.out.println("Java Class Path: "+System.getProperty("java.class.path")+"\n");
+ }
+}
